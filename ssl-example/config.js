ee0dad2a459edaf4a327dd0b2e3367770d9f033d
yacut <roman.rogozhnikov@gmail.com>
2017-08-20 13:01:04 +0200

    add eslint config and autofix all files

--- ssl-example/config.js
+++ ssl-example/config.js
@@ -2,37 +2,37 @@ const fs = require("fs");
 const config = {
   kafkaHost: "localhost:9093",
   logger: {
-      debug: _ => {},
-      info: msg => console.log(msg),
-      warn: msg => console.log(msg),
-      error: msg => console.log(msg)
+    debug: _ => {},
+    info: msg => console.log(msg),
+    warn: msg => console.log(msg),
+    error: msg => console.log(msg)
   },
   groupId: "example-group",
   clientName: "example-name",
   workerPerPartition: 1,
   options: {
-      ssl: true,
-      sslOptions: {
-        // https://nodejs.org/dist/latest-v8.x/docs/api/tls.html#tls_tls_createsecurecontext_options
-        rejectUnauthorized: true,
-        key: fs.readFileSync("../kafka-setup/certs/ca-key"),
-        cert: fs.readFileSync("../kafka-setup/certs/ca-cert"),
-        ca:[fs.readFileSync("../kafka-setup/certs/ca-cert")],
-        passphrase: "nodesinek"
-      },
-      sessionTimeout: 8000,
-      protocol: ["roundrobin"],
-      fromOffset: "latest",
-      fetchMaxBytes: 1024 * 1024,
-      fetchMinBytes: 1,
-      fetchMaxWaitMs: 10,
-      heartbeatInterval: 250,
-      retryMinTimeout: 250,
-      autoCommit: true,
-      autoCommitIntervalMs: 1000,
-      requireAcks: 1,
-      ackTimeoutMs: 100,
-      partitionerType: 3
+    ssl: true,
+    sslOptions: {
+      // https://nodejs.org/dist/latest-v8.x/docs/api/tls.html#tls_tls_createsecurecontext_options
+      rejectUnauthorized: true,
+      key: fs.readFileSync("../kafka-setup/certs/ca-key"),
+      cert: fs.readFileSync("../kafka-setup/certs/ca-cert"),
+      ca:[fs.readFileSync("../kafka-setup/certs/ca-cert")],
+      passphrase: "nodesinek"
+    },
+    sessionTimeout: 8000,
+    protocol: ["roundrobin"],
+    fromOffset: "latest",
+    fetchMaxBytes: 1024 * 1024,
+    fetchMinBytes: 1,
+    fetchMaxWaitMs: 10,
+    heartbeatInterval: 250,
+    retryMinTimeout: 250,
+    autoCommit: true,
+    autoCommitIntervalMs: 1000,
+    requireAcks: 1,
+    ackTimeoutMs: 100,
+    partitionerType: 3
   }
 };
 
--- ssl-example/producer.js
+++ ssl-example/producer.js
@@ -6,9 +6,9 @@ const producer = new Producer(require("./config.js"), ["test"], 1);
 producer.on("error", error => console.error(error));
 
 producer.connect().then(_ => {
-    console.log("connected.");
-    setInterval(() => {
-        console.log("send");
-        producer.send("test", "abc123");
-    }, 1000);
+  console.log("connected.");
+  setInterval(() => {
+    console.log("send");
+    producer.send("test", "abc123");
+  }, 1000);
 }).catch(error => console.error(error));
